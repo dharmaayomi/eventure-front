2d164c56a916f339e571cfc21f81e855d1b4dc27
pip-po <officialomicumi@gmail.com>
2025-04-29 16:29:16 +0700

    chart

--- src/features/(dashboard)/dashboard/_components/DashboardList.tsx
+++ src/features/(dashboard)/dashboard/_components/DashboardList.tsx
@@ -1,9 +1,11 @@
+import { ChartTransaction } from "./ChartTransaction";
+
 const DashboardList = () => {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="bg-muted/50 aspect-video rounded-xl">
-          cobain ini dluuu
+          {/* <ChartTransaction /> */}
         </div>
         <div className="bg-muted/50 aspect-video rounded-xl"></div>
         <div className="bg-muted/50 aspect-video rounded-xl"></div>
--- src/hooks/api/event/useGetEventsLocation.ts
+++ src/hooks/api/event/useGetEventsLocation.ts
@@ -8,7 +8,7 @@ const useGetEventsLocation = (slug: string) => {
     queryKey: ["events", slug],
     queryFn: async () => {
       const response = await axios.get<PageableResponse<Event>>(
-        `http://localhost:8000/events/location/${slug}`,
+        `http://localhost:9000/events/location/${slug}`,
       );
       return response.data;
     },
