eb0473cd6d0e28c4848207cf3ed852416cd55400
diarifiana <diarifiana@gmail.com>
2025-04-29 15:28:24 +0700

    feat: get detail page

--- src/hooks/api/event/useGetEvents.ts
+++ src/hooks/api/event/useGetEvents.ts
@@ -1,9 +1,9 @@
 import { Event } from "@/types/event";
-import { PageableResponse } from "@/types/pagination";
+import { PageableResponse, PaginationQueries } from "@/types/pagination";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const useGetEvents = () => {
+const useGetEvents = (queries?: PaginationQueries) => {
   return useQuery<PageableResponse<Event>>({
     queryKey: ["events"],
     queryFn: async () => {
