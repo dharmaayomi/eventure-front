94da56680ff7d9174f2e63eb100b7a2253b00c83
diarifiana <diarifiana@gmail.com>
2025-05-03 15:55:42 +0700

    feat: create tx

--- src/hooks/api/event/useGetEvent.ts
+++ src/hooks/api/event/useGetEvent.ts
@@ -1,14 +1,14 @@
+import useAxios from "@/hooks/useAxios";
 import { Event } from "@/types/event";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 
 const useGetEvent = (slug: string) => {
+  const { axiosInstance } = useAxios();
+
   return useQuery({
     queryKey: ["event", slug],
     queryFn: async () => {
-      const { data } = await axios.get<Event>(
-        `${process.env.NEXT_PUBLIC_BASE_API_URL}/events/${slug}`,
-      );
+      const { data } = await axiosInstance.get<Event>(`/events/${slug}`);
       return data;
     },
   });
